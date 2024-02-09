a5c24d912c0ad43c47302a8908b7a3a000164edf
Dean Lynch <lynchdean@gmail.ccom>
2024-02-09 20:15:49 +0000

    Basic page functionality

--- viewer/urls.py
+++ viewer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:file_id>/", views.file_view, name="file view"),
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import File
 
 def index(request):
-    return HttpResponse("viewer index.")
+    file_list = File.objects.order_by("title")
+    context = {"file_list": file_list}
+    return render(request, "viewer/index.html", context)
+
+def file_view(request, file_id):
+    try:
+        file = File.objects.get(pk=file_id)
+    except File.DoesNotExist:
+        raise Http404("File does not exist")
+    return render(request, "viewer/file.html", {"file":file})
