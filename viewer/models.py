d366d9bf48d2a1a57ab4225acbdab99a49390f39
Dean Lynch <lynchdean@gmail.ccom>
2024-02-09 15:42:07 +0000

    add file model

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    title = models.CharField(max_length=100)
+    file_name = models.CharField(max_length=100)
+    
